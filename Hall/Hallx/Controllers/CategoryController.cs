3ee900627d932c06fa064f29d98368bb7a064c17
CHUKWUDI INNOCENT <125125776+Innochu@users.noreply.github.com>
2024-05-12 15:00:40 +0100

    projectsetup/bootswatch/categorycontroller/Dbcontext/entity

---
+++ Hall/Hallx/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Hallx.Data;
+using Hallx.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hallx.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly HallxDbContext _hallxDbContext;
+
+        public CategoryController(HallxDbContext hallxDbContext)
+        {
+            _hallxDbContext = hallxDbContext;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = _hallxDbContext.Categories.ToList();
+            return View(categories);
+        }
+    } 
+}
---
+++ Hall/Hallx/Data/HallxDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Hallx.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hallx.Data
+{
+    public class HallxDbContext : DbContext
+    {
+        public HallxDbContext(DbContextOptions<HallxDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id=1, Name="Toys", DisplayOrder=6},
+                new Category { Id=2, Name="Soap", DisplayOrder=7},
+                new Category { Id=3, Name="Hamper", DisplayOrder=9});
+
+        }
+    }
+}
