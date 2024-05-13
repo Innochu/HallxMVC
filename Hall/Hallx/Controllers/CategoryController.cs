435b299e9457d2de06fd5832f68a6ce830ad9eac
CHUKWUDI INNOCENT <125125776+Innochu@users.noreply.github.com>
2024-05-13 02:55:59 +0100

    edit/delete

--- Hall/Hallx/Controllers/CategoryController.cs
+++ Hall/Hallx/Controllers/CategoryController.cs
@@ -22,5 +22,73 @@ namespace Hallx.Controllers
         {
         return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if(category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Name and Display Order cannot be the same.");
+            }
+            if(ModelState.IsValid)
+            {
+                _hallxDbContext.Categories.Add(category);
+                _hallxDbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+           return View();
+        }
+        public IActionResult Edit(int id) 
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            Category category = _hallxDbContext.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+        return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+           
+            if(ModelState.IsValid)
+            {
+                _hallxDbContext.Categories.Update(category);
+                _hallxDbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+           return View();
+        }
+        public IActionResult Delete(int id) 
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            Category category = _hallxDbContext.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+        return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int id)
+        {
+            Category category = _hallxDbContext.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+                _hallxDbContext.Categories.Remove(category);
+                _hallxDbContext.SaveChanges();
+                return RedirectToAction("Index");
+        }
     } 
 }
--- Hall/Hallx/Models/Category.cs
+++ Hall/Hallx/Models/Category.cs
@@ -8,9 +8,11 @@ namespace Hallx.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(10)]
         [DisplayName("Category Name")]
         public string Name { get; set; } = string.Empty;
         [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage ="Order must be between 1-100")]
         public int DisplayOrder { get; set; }
     }
 }
